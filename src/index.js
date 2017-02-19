264baab007d0f96eb8acb3ad872c41b2714beca5
Myles O'Connor <hello@myles.la>
2017-02-19 11:18:52 -0800

    Fixed basic auth routes, added ESlint to project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React, { createClass } from 'react'
-import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory, Link, withRouter } from 'react-router'
-
-import withExampleBasename from './helpers/withExampleBasename'
+/*global document b:true*/
+import React, { createClass } from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory, Link, withRouter } from 'react-router';
 
 function App(props) {
     return (
         <div>
             { props.children }
         </div>
-    )
+    );
 }
 
 const Form = withRouter(
@@ -17,56 +16,56 @@ const Form = withRouter(
         getInitialState() {
             return {
                 value: ''
-            }
+            };
         },
         submitAction(event) {
-            event.preventDefault()
+            event.preventDefault();
             this.props.router.push({
                 pathname: '/page',
                 query: {
                     qsparam: this.state.value
                 }
-            })
+            });
         },
         handleChange(event) {
-            this.setState({ value: event.target.value })
+            this.setState({ value: event.target.value });
         },
         render() {
             return (
-                <form onSubmit={ this.submitAction }>
+                <form onSubmit={this.submitAction}>
                     <p>Token is <em>pancakes</em></p>
-                    <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
                     <button type="submit">Submit the thing</button>
                     <p><Link to="/page?qsparam=pancakes">Or authenticate via URL</Link></p>
                     <p><Link to="/page?qsparam=bacon">Or try failing to authenticate via URL</Link></p>
                 </form>
-            )
+            );
         }
     })
-)
+);
 
 function Page() {
-    return <h1>Hey, I see you are authenticated. Welcome!</h1>
+    return <h1>Hey, I see you are authenticated. Welcome!</h1>;
 }
 
 function ErrorPage() {
-    return <h1>Oh no! Your auth failed!</h1>
+    return <h1>Oh no! Your auth failed!</h1>;
 }
 
 function requireCredentials(nextState, replace, next) {
-    const query = nextState.location.query
+    const query = nextState.location.query;
     if (query.qsparam) {
         serverAuth(query.qsparam)
         .then(
             () => next(),
             () => {
-                replace('/error')
-                next()
+                replace('/error');
+                next();
             }
-        )
+        );
     } else {
-        replace('/error')
-        next()
+        replace('/error');
+        next();
     }
 }
 
@@ -75,19 +74,19 @@ function serverAuth(authToken) {
         // That server is gonna take a while
         setTimeout(() => {
             if (authToken === 'pancakes') {
-            resolve('authenticated')
+            resolve('authenticated');
         } else {
-            reject('nope')
-        }}, 200);
+            reject('nope');
+        } }, 200);
     });
 }
 
 render((
-    <Router history={withExampleBasename(browserHistory, __dirname)}>
-        <Route path="/" component={ App }>
-            <IndexRoute component={ Form } />
-            <Route path="page" component={ Page } onEnter={ requireCredentials }/>
-            <Route path="error" component={ ErrorPage }/>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Form} />
+            <Route path="page" component={Page} onEnter={requireCredentials} />
+            <Route path="error" component={ErrorPage} />
         </Route>
     </Router>
 ), document.getElementById('root'));
